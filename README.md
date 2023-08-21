d340ce93fdd8cf67a29b0bdd89373bc44673deee
Ward Bekker <ward@wardbekker.com>
2023-08-21 15:54:36 +0200

    updates

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ This example shows how LLAMA can be used to summarize gong call transcripts
 
 ## Usage
 
-- Download LLAMA model from https://huggingface.co/TheBloke/Llama-2-13B-chat-GGML/resolve/main/llama-2-13b-chat.ggmlv3.q4_K_S.bin and place it in a ./models folder
+- Download LLAMA model from https://huggingface.co/TheBloke/Llama-2-13B-chat-GGML/resolve/main/llama-2-13b-chat.ggmlv3.q5_K_S.bin and place it in a ./models folder
 - Create a virtual environment: `python -m venv venv`
 - Activate the virtual environment: `source venv/bin/activate`
 - Install the requirements: `pip install -r requirements.txt`
+- On Mac M1, M2 etc make sure to build llama-cpp-python like this: `CMAKE_ARGS="-DLLAMA_METAL=on" FORCE_CMAKE=1 pip install --force llama-cpp-python`. This will build the Metal version of LLAMA which is much faster than the CPU version.
 - Run the summarization script: `python summarize.py <gong_transcript_html_file> <key_speaker_name> <output_file>`
 
 ## Example
@@ -15,3 +16,5 @@ This example shows how LLAMA can be used to summarize gong call transcripts
 ```
 python summarize.py gong-transcript.html "Ward" summary.txt
 ```
+
+(Main) Speaker name is used for semi-intelligent chunking of the text. 
--- summarize.py
+++ summarize.py
@@ -6,13 +6,13 @@ from llama_cpp import Llama
 CONTEXT = 4096 # 4k context length
 TOKEN_CHARACTER_RATIO = 2
 MAX_CHUNK_LENGTH = int(CONTEXT * TOKEN_CHARACTER_RATIO) - 200
-MODEL_NAME = "./models/llama-2-13b-chat.ggmlv3.q4_K_S.bin"
-LLAMA_PROMPT_TEMPLATE = """SYSTEM: You create summaries of business meeting transcript. Only respond in bulleted lines, in the line format: "* (Speaker Name) key points."
+MODEL_NAME = "./models/llama-2-13b-chat.ggmlv3.q5_K_S.bin"
+LLAMA_PROMPT_TEMPLATE = """SYSTEM: You create summaries of business meeting transcript. Do not repeat the actual conversations. Only respond in bulleted lines, in the line format. Summarize in the third person only, that is very important: "* (Speaker Name) key points."
 USER: INSERT_PROMPT_HERE
 ASSISTANT:"""
 
 # init the llama model
-llm = Llama(model_path=MODEL_NAME, n_ctx=CONTEXT, n_batch=768, n_gpu_layers=1, verbose=True)
+llm = Llama(model_path=MODEL_NAME, n_ctx=CONTEXT, n_batch=224, n_gpu_layers=1, verbose=True)
 
 def main():
     # if no command line arguments are given, print usage and exit
@@ -29,12 +29,21 @@ def main():
     # third command line value is the summary file
     summary_file = os.sys.argv[3]
     
-    summerize_chunks(parse_gong_transcript(transcript_file, speaker_name), summary_file)
+    summarisations = summerize_chunks(parse_gong_transcript(transcript_file, speaker_name), summary_file)
+
+    # TODO: support more chunks in the final summerisation step
+    overall_summary = summarize(chunk_text("".join(summarisations), speaker_name)[0])
+
+    # write overall summary to the summary file
+    with open(summary_file, "a") as f:
+        f.write("\n\nOverall summary:\n\n")
+        f.write(overall_summary)
+    print("done")
+
 
 # function that will summarize a piece of text 
 def summarize(text):    
-    #prompt = 'Summarize the following partial meeting transcription of a business conversation with Tom Wilkie, CTO of Grafana Labs, and a customer of Grafana Labs in a paragraph. List the customer questions. Mention any speech organisation patterns like problem-solution. Fix spelling errors:\n' + text
-    prompt = 'Summarize the key points of the following business meeting transcript: ' + text
+    prompt = 'Summarize the key points of the following business meeting transcript as a flat list of lines. This is very important.: ' + text
     prompt = LLAMA_PROMPT_TEMPLATE.replace('INSERT_PROMPT_HERE', prompt)
     output = llm(prompt, max_tokens=-1, echo=False, temperature=0.7, top_p=0.1, top_k=40, repeat_penalty=1.176)
     llm.reset()
@@ -42,19 +51,7 @@ def summarize(text):
     # remove text until the semicolon, to remove stuff like: Sure! Here is a summary of the conversation in a single sentence:
     return text[text.find(":") + 1:]
 
-def parse_gong_transcript(transcript_file, speaker_name):
-    html = ""
-
-    with open(transcript_file, "r") as f:
-        html = f.read()
-
-    # extract all the divs with the class "monologue"
-    # monologues = re.findall(r'<div class="monologue">(.+?)</div>', html, re.DOTALL)
-    monologues = re.findall(r'<div class="monologue">(.+?)</div>', html, re.DOTALL)
-
-    # remove any remaining html tags with a regex
-    text = re.sub('<[^<]+?>', '', " ".join(monologues))
-
+def chunk_text(text, speaker_name):
     # remove whitespace longer than 2 characters
     text = re.sub(r'\s{2,}', ' ', text)
 
@@ -70,7 +67,25 @@ def parse_gong_transcript(transcript_file, speaker_name):
         text = text[split_index + 1:]
 
     chunks.append(text)
+    
+    # print the amount of chunks generated
+    print(f"generated {len(chunks)} chunks")
     return chunks
+
+def parse_gong_transcript(transcript_file, speaker_name):
+    html = ""
+
+    with open(transcript_file, "r") as f:
+        html = f.read()
+
+    # extract all the divs with the class "monologue"
+    # monologues = re.findall(r'<div class="monologue">(.+?)</div>', html, re.DOTALL)
+    monologues = re.findall(r'<div class="monologue">(.+?)</div>', html, re.DOTALL)
+
+    # remove any remaining html tags with a regex
+    text = re.sub('<[^<]+?>', '', " ".join(monologues))
+
+    return chunk_text(text, speaker_name)
 
 def summerize_chunks(chunks, target_file):
     # let's summarize the conversation per chunk
@@ -93,7 +108,7 @@ def summerize_chunks(chunks, target_file):
         
         for i, sum in enumerate(summarisations):
             f.write(sum)
-    print("done")
+    return summarisations
 
 if __name__ == "__main__":
     main()
